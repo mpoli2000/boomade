0a76b97006688be92f0be62f54c426019500fee0
Martin Policichio <martinpolicichio@yahoo.com.ar>
2023-08-17 14:09:14 -0300

    settings updated

--- boomade/settings.py
+++ boomade/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-(xx5t-x9w9ujt_1z*-e&%@l)6-rjqc8zu4z-^bwu9gq*2^jtfe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'boomade.ingenios.com.ar']
+CSRF_TRUSTED_ORIGINS = ['https://boomade.ingenios.com.ar']
 
 
 # Application definition
@@ -118,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
